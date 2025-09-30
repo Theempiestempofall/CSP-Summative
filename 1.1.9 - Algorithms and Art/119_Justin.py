1acac8effee03000c79d9adb1d963d54133fde0f
Theempiestempofall <303458@shakopeeschools.org>
2025-09-30 10:08:01 -0500

    Update 119_Justin.py
    Finished the project's code by importing random and added the code to delete points

--- 1.1.9 - Algorithms and Art/119_Justin.py
+++ 1.1.9 - Algorithms and Art/119_Justin.py
@@ -1,13 +1,29 @@
 import turtle as trtl
+import random
 #ask the user questions
 #set the answers to varibles 
 #make turtle draw a shape the changes depending on what the user said
 shape_color = input ("What is you favorite color?")
 shape_num_edges = input ("Pick a number between 1 and 10")
-shape_edges_list = ((0, 0), (0, 50), (10, 40), (25, 25), (40, 10), (50, 0), (40, -10), (25, -25), (10, -40), (0, -50), (-50, 0))
-trtl.addshape( "polygon", shape_edges_list )
+shape_edges_list = [
+    (0, 0),
+    (0, 50), 
+    (80, 40), 
+    (25, 25), 
+    (40, 10), 
+    (50, 0), 
+    (40, -100), 
+    (5, -25), 
+    (10, -40), 
+    (0, -50), 
+    (-50, 0)]
+for i in range (int(shape_num_edges)):
+    shape_edges_list.remove(random.choice(shape_edges_list))
+shape_edges_tuple = tuple(shape_edges_list)
+trtl.addshape( "polygon", shape_edges_tuple )
 
 pg = trtl.Turtle(shape = "polygon")
+pg.color(shape_color)
 
 wn = trtl.Screen()
 wn.mainloop()
