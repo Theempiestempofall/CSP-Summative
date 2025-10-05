e5c8e35c87cb7461ddcbd006cdf72e45a80daf9b
Theempiestempofall <303458@shakopeeschools.org>
2025-10-05 18:56:01 -0500

    updated physic sim
    Make the code more concise and explained it

--- 1.1.9 - Algorithms and Art/physic_sim.py
+++ 1.1.9 - Algorithms and Art/physic_sim.py
@@ -1,7 +1,8 @@
 import turtle
 
-start_x_pos = -350
-start_y_pos = 50
+#define varibles and get user input
+x_pos = -350
+y_pos = 50
 turtle.setup(width=800, height=600)
 turtle.bgcolor("lightblue")
 turtle.textinput("Physics Simulation", "Press OK to start the simulation.")
@@ -9,17 +10,15 @@ gravity = turtle.textinput("Gravity factor", "Enter a gravity factor (suggested:
 x_start_velocity = turtle.textinput("velocity factor", "Enter a horizontal velocity factor (suggested: 10 to 100)")
 iteration_distance = turtle.textinput("iteration distance", "Enter the distance between iterations (suggested: 0.5 to 2)")
 ball = turtle.Turtle(shape="circle")
-ball.teleport(start_x_pos, start_y_pos)
+ball.teleport(x_pos, y_pos)
 ball.color("red")
 ball.shapesize(stretch_wid=0.2, stretch_len=0.2)
+#simulate physics
 for i in range(200):
-    x_pos = ball.xcor()
-    y_pos = ball.ycor()
-    new_x_pos = x_pos + ((float(x_start_velocity)) * (float(iteration_distance)))
-    new_y_pos = y_pos - ((float(gravity) * (float(iteration_distance) * i))) * float(iteration_distance)
-    ball.goto(new_x_pos, new_y_pos)
+    x_pos = x_pos + ((float(x_start_velocity)) * (float(iteration_distance)))
+    y_pos = y_pos - ((float(gravity) * (float(iteration_distance) * i))) * float(iteration_distance)
     ball.stamp()
-    turtle.delay(0)
+    ball.goto(x_pos, y_pos)
     turtle.update()
 
 
