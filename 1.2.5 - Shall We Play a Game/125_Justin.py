19b247bf7c69a47eccb2e0691a2dfda711ecb542
Theempiestempofall <303458@shakopeeschools.org>
2025-10-27 10:44:34 -0500

    Wrote comments
    Wrote comments that decompose what I have to do in the program

--- 1.2.5 - Shall We Play a Game/125_Justin.py
+++ 1.2.5 - Shall We Play a Game/125_Justin.py
@@ -25,22 +25,24 @@ bet = 0
 timer = 3
 timer_up = False
 #---------Function Setup---------------
-def update_bet_display():
-    bet_writer.clear()
-    bet_writer.write("Bet: $" + str(bet), align='center', font=("Arial", 30, "normal"))
+# Display money
 def update_money_display():
     money_writer.clear()
     money_writer.write("Money: $" + str(money), align='center', font=("Arial", 30, "normal"))
+# Showing bets
+def update_bet_display():
+    bet_writer.clear()
+    bet_writer.write("Bet: $" + str(bet), align='center', font=("Arial", 30, "normal"))
 def place_bet():
     global bet
     bet = wn.numinput("Place your bet", "You have $" + str(money) + ". How much would you like to bet?", minval=1, maxval=money)
     update_bet_display()
-
+#Spinning the slots
 def spin_slots():
     global money, bet, timer, timer_up
     timer = 3
     print("Slots are spinning")
-    while timer_up == False:
+    if timer_up == False:
         Slot1Pen.color(Possible_colors[rand.randint(0,len(Possible_colors)-1)])
         Slot1Pen.shape(Possible_shapes[rand.randint(0,len(Possible_shapes)-1)])
         Slot2Pen.color(Possible_colors[rand.randint(0,len(Possible_colors)-1)])
@@ -49,28 +51,31 @@ def spin_slots():
         Slot3Pen.shape(Possible_shapes[rand.randint(0,len(Possible_shapes)-1)])
     check_win()
     update_money_display()
+#Check if you win
 def check_win():
     global money, bet
     if Slot1Pen.shape() == Slot2Pen.shape() == Slot3Pen.shape() and Slot1Pen.color() == Slot2Pen.color() == Slot3Pen.color():
         money = money + (bet * 10)
-        trtl.textinput("You Win!", "Congratulations! You won $" + str(bet * 10) + "! You now have $" + str(money) + ". Press Enter to continue.")
+        trtl.textinput("You Win!", "Congratulations! You won $" + str(bet * 2) + "! You now have $" + str(money) + ". Press Enter to continue.")
     elif Slot1Pen.shape() == Slot2Pen.shape() == Slot3Pen.shape() or Slot1Pen.color() == Slot2Pen.color() == Slot3Pen.color():
         money = money + (bet * 5)
-        trtl.textinput("You Win!", "Congratulations! You won $" + str(bet * 5) + "! You now have $" + str(money) + ". Press Enter to continue.")
+        trtl.textinput("You Win!", "Congratulations! You won $" + str(bet * 1.5) + "! You now have $" + str(money) + ". Press Enter to continue.")
     elif Slot1Pen.shape() == Slot2Pen.shape() or Slot2Pen.shape() == Slot3Pen.shape() or Slot1Pen.shape() == Slot3Pen.shape() or Slot1Pen.color() == Slot2Pen.color() or Slot2Pen.color() == Slot3Pen.color() or Slot1Pen.color() == Slot3Pen.color():
         money = money + (bet * 2)
-        trtl.textinput("You Win!", "Congratulations! You won $" + str(bet * 2) + "! You now have $" + str(money) + ". Press Enter to continue.")
+        trtl.textinput("You Win!", "Congratulations! You won $" + str(bet * 1.2) + "! You now have $" + str(money) + ". Press Enter to continue.")
     else:
         money = money - bet
         trtl.textinput("You Lose!", "Sorry, you lost your bet of $" + str(bet) + ". You now have $" + str(money) + ". Press Enter to continue.")
-    if money == 0:
+    if money == 0:# Ending the program if you have no money
         trtl.textinput("Game Over", "You have run out of money! Game Over! Press Enter to exit.")
         wn.bye()
 def countdown():# making it look like a slot machine roling down
     global timer, timer_up
     if timer <= 0:
+        print("Time is stopped")
         timer_up = True
     else:
+        print("time is moving")
         timer -= 1
 #---------Game Loop---------------
 spin_slots()
@@ -79,5 +84,6 @@ spin_slots()
 wn.onkeypress(place_bet, "b")
 wn.onkeypress(spin_slots, "r")
 wn.ontimer(countdown, 1000)# I think 1000 is a second but idk
+
 wn.listen()
 wn.mainloop()
